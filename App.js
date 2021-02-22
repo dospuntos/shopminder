604388e10bfaa65aeef3af9d1bc896de36d43cc8
johan@dospuntos.no <johan@dospuntos.no>
2021-02-22 12:33:32 -0400

    Basic login screen

--- App.js
+++ App.js
@@ -1,43 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { db } from "./firebase";
-
+import React, { useState } from "react";
+import { Text } from "react-native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { auth } from "./firebase";
+import LoginScreen from "./screens/sessions/LoginScreen";
+const Stack = createStackNavigator();
 export default function App() {
-  const [shopminders, setShopminders] = useState([]);
-
-  useEffect(() => {
-    const ref = db.collection("shopminders");
-
-    ref.onSnapshot((query) => {
-      const objs = [];
-      query.forEach((doc) => {
-        objs.push({
-          id: doc.id,
-          ...doc.data(),
-        });
-      });
-      setShopminders(objs);
-    });
-  }, []);
-
+  const [signedIn, setSignedIn] = useState(false);
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setSignedIn(true);
+    } else {
+      setSignedIn(false);
+    }
+  });
   return (
-    <View style={styles.container}>
-      {shopminders.map((obj) => (
-        <View id={obj.id}>
-          <Text>{obj.name}</Text>
-        </View>
-      ))}
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={DefaultTheme}>
+      {signedIn ? (
+        <Text>Signed in</Text>
+      ) : (
+        <>
+          <StatusBar style="light" />
+          <Stack.Navigator mode="card" screenOptions={{}}>
+            <Stack.Screen
+              name="signIn"
+              component={LoginScreen}
+              options={{
+                title: "Sign in",
+                headerStyle: {
+                  backgroundColor: "#29434e",
+                  borderBottomColor: "#29434e",
+                },
+                headerTintColor: "#fff",
+              }}
+            />
+          </Stack.Navigator>
+        </>
+      )}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

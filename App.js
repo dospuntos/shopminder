938d2a34bde492b99d4130b75bdf384ce1ef6511
johan@dospuntos.no <johan@dospuntos.no>
2021-02-22 09:52:59 -0400

    Confirm connection to firebase

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { db } from "./firebase";
 
 export default function App() {
+  const [shopminders, setShopminders] = useState([]);
+
+  useEffect(() => {
+    const ref = db.collection("shopminders");
+
+    ref.onSnapshot((query) => {
+      const objs = [];
+      query.forEach((doc) => {
+        objs.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setShopminders(objs);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {shopminders.map((obj) => (
+        <View id={obj.id}>
+          <Text>{obj.name}</Text>
+        </View>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
